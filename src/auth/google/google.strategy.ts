0787d9e9883d9a22755724f5f6472711e86a9486
Gwonyeong <rnjsdud980@naver.com>
2023-03-06 15:19:14 +0900

    Fix: dev ecv

--- src/auth/google/google.strategy.ts
+++ src/auth/google/google.strategy.ts
@@ -25,7 +25,6 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     profile: any,
     done: VerifyCallback,
   ): Promise<any> {
-    console.log(profile);
     const { emails, photos, id } = profile;
     const email = emails[0].value;
     const profileImage = photos[0];
